c0f5310dae23fe43f33ec38a35039a021698bb90
mdbernard <31671214+mdbernard@users.noreply.github.com>
2017-10-12 10:54:04 -0400

    testing reading yaml file

--- writeYaml.py
+++ writeYaml.py
@@ -1,5 +1,17 @@
 import yaml
-testyaml = open("/Users/michaelbernard/Documents/GitHub/ChemKED-database/2-butanol/2-butanol LPST Stranic xO2_0.03_phi_1.0.yaml", "r")
-printableYaml = yaml.load(testyaml)
-for line in printableYaml:
-    print(line)
+
+def yaml_loader(filepath):
+    """Loads a yaml file"""
+    with open(filepath) as file_descriptor:
+        data = yaml.load(file_descriptor)
+    return data
+
+def yaml_dump(filepath, data):
+    """Dumps data to a yaml file"""
+    with open(filepath, "w+") as file_descriptor:
+        yaml.dump(data, file_descriptor)
+
+if __name__ == "__main__":
+    filepath = "/Users/michaelbernard/Documents/GitHub/ChemKED-database/2-butanol/2-butanol LPST Stranic xO2_0.03_phi_1.0.yaml"
+    data = yaml_loader(filepath)
+    print(data)
