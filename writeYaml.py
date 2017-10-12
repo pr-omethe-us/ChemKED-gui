d359c14ed1ce29b8106e74be9ee309e7e76c8271
mdbernard <31671214+mdbernard@users.noreply.github.com>
2017-10-12 10:11:03 -0400

    testing opening files

--- writeYaml.py
+++ writeYaml.py
@@ -1,1 +1,5 @@
 import yaml
+testyaml = open("/Users/michaelbernard/Documents/GitHub/ChemKED-database/2-butanol/2-butanol LPST Stranic xO2_0.03_phi_1.0.yaml", "r")
+printableYaml = yaml.load(testyaml)
+for line in printableYaml:
+    print(line)
