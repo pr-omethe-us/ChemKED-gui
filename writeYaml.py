1c24583bee775231cbb53fb51a07154b896f7c3e
mdbernard <31671214+mdbernard@users.noreply.github.com>
2017-10-17 00:08:15 -0400

    Receive info via prompts
    Lines need to be grouped into functions

--- writeYaml.py
+++ writeYaml.py
@@ -19,7 +19,26 @@ detail = input("Detail: ")
 print("Experiment Information")
 experiment_type = input("Experiment Type: ")
 print("Apparatus Information")
-apparatus = input("Apparatus: ")
 kind = input("Kind: ")
 institution = input("Institution: ")
 facility = input("Facility: ")
+
+#print("Common Properties Information")
+#Ask Weber about formatting here
+
+print("Datapoints Information")
+num_datapoints = int(input("How many datapoints? "))
+# confirm = input(" ".join(["Is", num_datapoints, "correct? (y/n)"]))
+datapoints = []
+for i in range(num_datapoints):
+    print("Datapoint {}".format(i+1))
+    temperature = input("Temperature (K): ")
+    ignition_delay = input("Ignition Delay (us): ")
+    pressure = input("Pressure (atm): ")
+    # Ask Weber about the following:
+    # composition, ignition type
+    equivalence_ratio = input("Equivalence Ratio: ")
+    datapoint = [temperature, ignition_delay, pressure,
+                 equivalence_ratio]
+    datapoints.append(datapoint)
+print(datapoints)
