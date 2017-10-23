9659c8e952bd7ddb67905fc5776c87996e383411
mdbernard <31671214+mdbernard@users.noreply.github.com>
2017-10-23 15:30:51 -0400

    Testing buttons

--- chemkedGui.py
+++ chemkedGui.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 
 class window(QMainWindow):
 
@@ -8,14 +9,15 @@ class window(QMainWindow):
         super(window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("Still Testing")
-        self.setWindowIcon(QIcon('pic.png'))
+        # self.setWindowIcon(QIcon('pic.png'))
+        self.home()
+
+    def home(self):
+        btn = QPushButton('quit', self)
+        btn.clicked.connect(QCoreApplication.instance().quit)
+        btn.resize(100, 100)
+        btn.move(100, 100)
         self.show()
-
-
-# app = QApplication(sys.argv)
-# window = QWidget()
-# window.setGeometry(50, 50, 500, 300)
-# window.setWindowTitle("It\'s Just A Test")
 
 
 def main():
