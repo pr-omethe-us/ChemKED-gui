33b99dee49cd06eebe66655f14ce8dd915eebb0b
mdbernard <31671214+mdbernard@users.noreply.github.com>
2017-10-23 15:14:35 -0400

    Learning PyQt5 GUI

--- chemkedGui.py
+++ chemkedGui.py
@@ -1,9 +1,27 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+class window(QMainWindow):
+
+    def __init__(self):
+        super(window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("Still Testing")
+        self.setWindowIcon(QIcon('pic.png'))
+        self.show()
+
+
+# app = QApplication(sys.argv)
+# window = QWidget()
+# window.setGeometry(50, 50, 500, 300)
+# window.setWindowTitle("It\'s Just A Test")
 
 
 def main():
-    pass
+    app = QApplication(sys.argv)
+    Gui = window()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
