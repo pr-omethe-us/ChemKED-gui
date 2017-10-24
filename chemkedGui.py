62c1cc0df1a47358494c04c3a7e5af9a2522aefc
mdbernard <31671214+mdbernard@users.noreply.github.com>
2017-10-24 09:04:29 -0400

    Button Testing

--- chemkedGui.py
+++ chemkedGui.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QAction
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 
 class window(QMainWindow):
@@ -8,21 +10,27 @@ class window(QMainWindow):
     def __init__(self):
         super(window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle("Still Testing")
+        self.setWindowTitle("It\'s Just a Test")
+        # setWindowIcon does nothing on OSX. Works for Windows.
         # self.setWindowIcon(QIcon('pic.png'))
         self.home()
 
     def home(self):
         btn = QPushButton('quit', self)
-        btn.clicked.connect(QCoreApplication.instance().quit)
-        btn.resize(100, 100)
-        btn.move(100, 100)
+        btn.clicked.connect(self.close_application)
+
+        btn.resize(btn.sizeHint())
+        btn.move(0, 0)
         self.show()
+
+    def close_application(self):
+        print("Passed Test")
+        sys.exit()
 
 
 def main():
     app = QApplication(sys.argv)
-    Gui = window()
+    gui = window()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
