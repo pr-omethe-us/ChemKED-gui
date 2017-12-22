fbc914681e14dec38f02d2144fe0c189ca89b3ab
mdbernard <31671214+mdbernard@users.noreply.github.com>
2017-12-22 15:21:19 -0500

    Updated gitignore, updated file names, added testing files

--- guiv1.1.py
+++ guiv1.1.py
@@ -8,6 +8,9 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow,
 from pyked import chemked
 
 
+# These are the variables that will be exported to a ChemKED file
+
+
 class WindowFrame(QMainWindow):
     """
     Controls main window handling.
@@ -43,18 +46,6 @@ class WindowFrame(QMainWindow):
         # Move window to frame
         self.move(frame.topLeft())
 
-    # def closeEvent(self, event):
-    #     """
-    #     Prompts the user before closing the window.
-    #     """
-    #     reply = QMessageBox.Question(self, 'Message', 'Are you sure you want to quit?',
-    #                                  QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
-    #
-    #     if reply == QMessageBox.Yes:
-    #         event.accept()
-    #     else:
-    #         event.ignore()
-
 
 class Tabs(QWidget):
     """
@@ -64,6 +55,7 @@ class Tabs(QWidget):
 
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
+
         self.layout = QVBoxLayout(self)
         self.tabs = QTabWidget()
 
@@ -81,53 +73,49 @@ class Tabs(QWidget):
     def tab_1_setup(self):
         self.tab1 = QWidget()  # This is the returned QWidget
 
-        meta_authors = []
-        meta_orcids = []
-        meta_values = [file_version, chemked_version] = \
-                      [QLineEdit('0'), QLineEdit('0.0.1')]
-        meta_labels = ['File Version', 'ChemKED Version']
+        meta = QVBoxLayout()  # All meta section content added to this box
 
-        ref_authors = []
-        ref_orcids = []
-        ref_values = [doi, ref_authors, journal, year, volume, pages, detail] = \
-                     [QLineEdit(), QLineEdit(), QLineEdit(), QLineEdit(), QLineEdit(), QLineEdit(), QLineEdit()]
-        ref_labels = ['DOI', 'Author(s)', 'Journal', 'Year', 'Volume', 'Pages', 'Detail']
+        self.tab1.authors_box = QVBoxLayout()  # Additional authors added to this box
+        self.tab1.btnsRemoveMetaAuthors = []  # List of buttons to remove individual meta authors
+        self.tab1.metaAuthors = []  # Contains lists of author name/orcid pairs
 
-        tab_1_vbox = QVBoxLayout()
-        tab_1_hbox = QHBoxLayout()
+        # This is the hard-coded required author (first row, no remove button)
+        required_author = QHBoxLayout()
+        required_author_name = QLineEdit()
+        required_author_orcid = QLineEdit()
+        required_author_name_form = QFormLayout()
+        required_author_orcid_form = QFormLayout()
+        required_author_name_form.addRow(QLabel('Author'), required_author_name)
+        required_author_orcid_form.addRow(QLabel('ORCID'), required_author_orcid)
+        required_author.addLayout(required_author_name_form)
+        required_author.addLayout(required_author_orcid_form)
+        add_author_button = QPushButton('Add...')
+        add_author_button.clicked.connect(self.add_meta_author)
+        required_author.addWidget(add_author_button)
 
-        meta_auths_box = QVBoxLayout()
-        meta_form = QFormLayout()
-        ref_form_header = QFormLayout()
-        ref_form_auth_box = QVBoxLayout()
-        ref_form_footer = QFormLayout()
+        self.tab1.authors_box.addLayout(required_author)
+        meta.addLayout(self.tab1.authors_box)
 
-        meta_auth_1_hbox = QHBoxLayout()
-        meta_auth_1_auth = QLineEdit()
-        meta_auth_1_orcid = QLineEdit()
-        meta_auth_1_auth_form = QFormLayout()
-        meta_auth_1_auth_form.addRow(QLabel('Author'), meta_auth_1_auth)
-        meta_auth_1_orcid_form = QFormLayout()
-        meta_auth_1_orcid_form.addRow(QLabel('ORCID'), meta_auth_1_orcid)
-        meta_auth_1_hbox.addLayout(meta_auth_1_auth_form)
-        meta_auth_1_hbox.addLayout(meta_auth_1_orcid_form)
-
-        meta_auths_box.addLayout(meta_auth_1_hbox)
-        tab_1_vbox.addLayout(meta_auths_box)
-
-        for label, value in zip(meta_labels, meta_values):
-            meta_form.addRow(QLabel(label), value)
-
-        # for label, value in zip(ref_labels, ref_values):
-        #     ref_form.addRow(QLabel(label), value)
-
-        tab_1_hbox.addLayout(meta_form)
-        # tab_1_hbox.addLayout(ref_form)
-
-        tab_1_vbox.addLayout(tab_1_hbox)
-        self.tab1.setLayout(tab_1_vbox)
+        self.tab1.setLayout(meta)
 
         return self.tab1
+
+    def add_meta_author(self):
+        new_author = QLineEdit()
+        new_orcid = QLineEdit()
+        self.tab1.metaAuthors.append([new_author, new_orcid])
+
+        new_author_row = QHBoxLayout()
+
+        new_author_form = QFormLayout()
+        new_orcid_form = QFormLayout()
+        new_author_form.addRow(QLabel('Author'), new_author)
+        new_orcid_form.addRow(QLabel('ORCID'), new_orcid)
+
+        new_author_row.addLayout(new_author_form)
+        new_author_row.addLayout(new_orcid_form)
+
+        self.tab1.authors_box.addLayout(new_author_row)
 
     def tab_2_setup(self):
         self.tab2 = QWidget()
