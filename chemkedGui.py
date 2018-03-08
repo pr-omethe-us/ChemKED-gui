22079d3752279a9674607a6360777f347954062a
mdbernard <31671214+mdbernard@users.noreply.github.com>
2018-03-07 20:46:17 -0500

    Removed deepcopy import

--- chemkedGui.py
+++ chemkedGui.py
@@ -1,5 +1,4 @@
 import sys
-from copy import deepcopy
 from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow,
                              QPushButton, QMessageBox, QAction,
                              QHBoxLayout, QVBoxLayout, QTabWidget,
