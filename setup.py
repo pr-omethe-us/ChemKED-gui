a3fe73bfeec76b070aa3caac5b8771598ba48e65
Mike Bernard <michael.bernard@uconn.edu>
2018-04-01 00:24:59 -0400

    Reversed change from "_" to "-" in module names because that didn't work

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ install_requires = ['pyked>=0.4.0',
 python_requires = '>=3.5'
 
 setup(
-    name='chemked_gui',
+    name='chemked-gui',
     version='0.1.0',
     url='https://github.com/pr-omethe-us/ChemKED-gui',
     license='BSD 3-Clause',
     install_requires=install_requires,
     python_requires=python_requires,
     long_description=long_description,
-    packages=['chemked_gui'],
+    packages=['chemked-gui'],
+    package_dir={'chemked-gui': 'chemked-gui'},
     keywords=['chemical kinetics'],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -23,8 +24,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Chemistry',
     ],
